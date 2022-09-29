de0c955a51350cd237bdd315202a6009d024f5c8
Fernando Maia <fermaiasoares@aol.com>
2022-09-28 23:50:38 -0300

    Initial commit

---
+++ src/android/AutoUpdate.java
@@ -0,0 +1,49 @@
+package cordova.plugins.versasaude;
+
+import android.app.Activity;
+import android.content.Context;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.gms.tasks.Task;
+import com.google.android.play.core.appupdate.AppUpdateOptions;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.UpdateAvailability;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class AutoUpdate extends CordovaPlugin {
+
+    private AppUpdateManager appUpdateManager;
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("checkUpdate")) {
+            Context context = this.cordova.getContext();
+            Activity activity = this.cordova.getActivity();
+            appUpdateManager = AppUpdateManagerFactory.create(context);
+            Task<AppUpdateInfo> appUpdateInfoTask  = appUpdateManager.getAppUpdateInfo();
+
+            appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
+                if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                    appUpdateManager.startUpdateFlow(
+                            appUpdateInfo,
+                            activity,
+                            AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE)
+                                    .setAllowAssetPackDeletion(false)
+                                    .build()
+                    );
+                }
+            });
+        }
+        return false;
+    }
+}
