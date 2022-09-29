1f8510a1ca4841cdb0365550d2ea5ae6f394f6c0
Fernando Maia <fernando.maia@versatecnologia.com.br>
2022-09-29 15:19:35 -0300

    Retorno de mensagem

--- src/android/AutoUpdate.java
+++ src/android/AutoUpdate.java
@@ -27,22 +27,31 @@ public class AutoUpdate extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("checkUpdate")) {
-            Context context = this.cordova.getContext();
-            Activity activity = this.cordova.getActivity();
-            appUpdateManager = AppUpdateManagerFactory.create(context);
-            Task<AppUpdateInfo> appUpdateInfoTask  = appUpdateManager.getAppUpdateInfo();
+            try {
+                Context context = this.cordova.getContext();
+                Activity activity = this.cordova.getActivity();
 
-            appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
-                if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
-                    appUpdateManager.startUpdateFlow(
-                            appUpdateInfo,
-                            activity,
-                            AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE)
-                                    .setAllowAssetPackDeletion(false)
-                                    .build()
-                    );
-                }
-            });
+                appUpdateManager = AppUpdateManagerFactory.create(context);
+                Task<AppUpdateInfo> appUpdateInfoTask  = appUpdateManager.getAppUpdateInfo();
+
+                appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
+                    if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                        appUpdateManager.startUpdateFlow(
+                                appUpdateInfo,
+                                activity,
+                                AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE)
+                                        .setAllowAssetPackDeletion(false)
+                                        .build()
+                        );
+                    }
+                });
+
+                callbackContext.success("Atualização verificada com sucesso!");
+                return true;
+            } catch (Exception e) {
+                callbackContext.error("Não foi possível verificar a atualização");
+                return true;
+            }
         }
         return false;
     }
