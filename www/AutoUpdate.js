0810deeb3c87ec58d64089b51bb9186c1b8168dd
Fernando Maia <fernando.maia@versatecnologia.com.br>
2022-09-29 12:11:00 -0300

    Metodo padrão

--- www/AutoUpdate.js
+++ www/AutoUpdate.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'AppUpdate', 'coolMethod', [arg0]);
+exports.checkUpdate = function (arg0, success, error) {
+    exec(success, error, 'AppUpdate', 'checkUpdate', [arg0]);
 };
